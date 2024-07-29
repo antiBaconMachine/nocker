431fcec33123aaa0a1f084caa32c1f13be523c80
Ollie Edwards <oliver.edwards@doxy.me>
2024-07-29 15:57:30 +0100

    flesh out supertest version

--- index.spec.ts
+++ index.spec.ts
@@ -5,26 +5,6 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import request from "supertest";
 
 describe("Index", () => {
-  // it("should fake a response to a simple proxied endpoint", async () => {
-  //   nock("http://localhost:6666").post("/").reply(200, { data: "Hello World" });
-
-  //   const app = express();
-  //   app.use(createProxyMiddleware({ target: "http://localhost:6666" }));
-  //   const server = app.listen(9234, async () => {
-  //     try {
-  //       await axios.post("http://localhost:9234/").then((response) => {
-  //         expect(response.data).toEqual({ data: "Hello World" });
-  //       });
-  //     } finally {
-  //       return new Promise<void>((resolve) => {
-  //         server.close(() => {
-  //           resolve();
-  //         });
-  //       });
-  //     }
-  //   });
-  // });
-
   it("should fake a response to a simple proxied endpoint", async () => {
     nock("http://localhost:6666").post("/").reply(200, { data: "Hello World" });
 
@@ -39,78 +19,64 @@ describe("Index", () => {
       });
   });
 
-  // it("should fake a response to an endpoint when the method is changed", async () => {
-  //   const scope = nock("http://localhost:6666");
-  //   scope.post("/").reply(200, { data: "Hello World" });
-  //   scope.on("request", (req, interceptor, body) => {
-  //     console.log({
-  //       url: req.url,
-  //       method: req.method,
-  //       body,
-  //     });
-  //   });
-  //   const app = express();
-  //   app.use(
-  //     createProxyMiddleware({
-  //       target: "http://localhost:6666",
-  //       selfHandleResponse: true,
-  //       on: {
-  //         proxyReq: (proxyReq, req, res) => {
-  //           console.log(proxyReq.method);
-  //           proxyReq.setHeader("x-foo", "bar");
-  //           proxyReq.method = "POST";
-  //           console.log(proxyReq.method);
-  //           // proxyReq.end();
-  //         },
-  //         proxyRes: (proxyRes, req, res) => {
-  //           res.send({ data: "XXXXX World" });
-  //         },
-  //         error: (err, req, res) => {
-  //           console.error("NOOOOOOOO", err.message);
-  //         },
-  //       },
-  //     })
-  //   );
-  //   const server = app.listen(9235, async () => {
-  //     try {
-  //       await axios.get("http://localhost:9235/").then((response) => {
-  //         expect(response.data).toEqual({ data: "XXXXX World" });
-  //       });
-  //     } finally {
-  //       return new Promise(async (resolve) => {
-  //         server.close(() => {
-  //           resolve();
-  //         });
-  //       });
-  //     }
-  //   });
-  // });
+  it("should fake a response to an endpoint and fiddle with the response", async () => {
+    nock("http://localhost:6666").get("/").reply(200, { data: "Hello World" });
 
-  // it.skip("should fake a response to an endpoint when the method is changed", async () => {
-  //   const fakeTarget = express();
-  //   fakeTarget.all("*", (req, res) => {
-  //     console.log({ method: req.method });
-  //     res.send({ data: "Hello World" });
-  //   });
-  //   const proxyTargetReady = new Promise<void>((resolve) => {
-  //     const targetServer = fakeTarget.listen(9099, resolve);
-  //   });
+    const app = express();
+    app.use(
+      createProxyMiddleware({
+        target: "http://localhost:6666",
+        selfHandleResponse: true,
+        on: {
+          proxyRes: (proxyRes, req, res) => {
+            res.send({ data: "XXXXX World" });
+          },
+        },
+      })
+    );
 
-  //   const fakeApp = express();
-  //   fakeApp.use(createProxyMiddleware({ target: "http://localhost:9099" }));
-  //   const appReady = new Promise<void>((resolve) => {
-  //     const targetServer = fakeTarget.listen(5555, resolve);
-  //   });
+    request(app)
+      .get("/")
+      .expect(200)
+      .then((response: any) => {
+        expect(response.body).toEqual({ data: "XXXXX World" });
+      });
+  });
 
-  //   await Promise.all([proxyTargetReady, appReady]);
+  it("should fake a response to an endpoint when the method is changed", async () => {
+    const scope = nock("http://localhost:6666");
+    scope.post("/").reply(200, { data: "Hello World" });
+    scope.on("request", (req, interceptor, body) => {
+      console.log({
+        url: req.url,
+        method: req.method,
+        body,
+        headers: req.headers,
+      });
+    });
 
-  //   const response = await axios.get("http://localhost:5555/");
-  //   console.log(response.data);
+    const app = express();
+    app.use(
+      createProxyMiddleware({
+        target: "http://localhost:6666",
+        on: {
+          proxyReq: (proxyReq, req, res) => {
+            proxyReq.setHeader("x-foo", "bar");
+            proxyReq.method = "POST";
+            proxyReq.end();
+          },
+          error: (err, req, res) => {
+            console.log("ERROR", err);
+          },
+        },
+      })
+    );
 
-  //   await new Promise((resolve) => {
-  //     fakeTarget.close(() => {
-  //       resolve();
-  //     });
-  //   });
-  // });
+    await request(app)
+      .get("/")
+      .expect(200)
+      .then((response: any) => {
+        expect(response.body).toEqual({ data: "Hello World" });
+      });
+  });
 });
